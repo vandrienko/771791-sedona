3cf7d9448b599d4c65b8b57bb17a5acdfce98004
vandrienko <vandrienko1991@gmail.com>
2019-01-06 18:56:00 +0300

    Д20.Критическая функциональность сайта работоспособна без JavaScript (использовано прогрессивное улучшение). Исправил.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,21 @@ var form_search = document.querySelector(".search");
 var form = document.querySelector(".search-form");             
 var date_arrival = form.querySelector("[name=date-arrival]");           
 var date_departure = form.querySelector("[name=date-departure]");            
+var map_img = document.querySelector(".map-img");  
+var map_img_activ = document.querySelector(".map-img-activ");  
+
+    map_img.classList.add("map-img-hide");
+    map_img_activ.classList.add("map-img-show");
+    
+if (link_search) {
+    form_search.classList.add("search-hide");
+};
 
 link_search.addEventListener("click", function (evt) {
     evt.preventDefault();
     form_search.classList.toggle("modal-show");  
     form.classList.remove("modal-error");              
-  });  
+});  
 
 form.addEventListener("submit", function (evt) {
     if (!date_arrival.value || !date_departure.value) {   
